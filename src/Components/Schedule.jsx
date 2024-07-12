27e9b96ebf29ab28efa330e5453808b11bb763ab
Mohhit6075 <mohitsingh6266@gmail.com>
2024-07-12 15:53:19 +0530

    Update Schedule.jsx

--- src/Components/Schedule.jsx
+++ src/Components/Schedule.jsx
@@ -1,6 +1,9 @@
 const Schedule = () => {
   return (
-    <div id="schedule" className="w-9/12 mx-auto mt-24 flex flex-col gap-16 pb-20">
+    <div
+      id="schedule"
+      className="w-9/12 mx-auto mt-24 flex flex-col gap-16 pb-20"
+    >
       <h1 className="text-[#897be2] text-center text-[45px] uppercase font-bold">
         Schedule
       </h1>
@@ -10,7 +13,7 @@ const Schedule = () => {
         </div>
         <div className="absolute top-6 border border-[#07F8EA] rounded-lg w-[500px] h-[180px] p-4">
           <h1 className="text-[#07F8EA] text-3xl relative">Registration</h1>
-          <p className="text-[#ccc] text-[13px] mt-4">
+          <p className="text-[#ccc] text-[13px] mt-4 font-normal">
             Version Beta is opening its doors for applications starting from
             August 20th until October 5th.To apply, all you need to do is to
             click on the “Register Here” Button above and fill the registration
@@ -21,7 +24,7 @@ const Schedule = () => {
         </div>
         <div className="absolute right-0 top-64 translate-y-6 border border-[#07F8EA] rounded-lg w-[500px] h-[180px] p-4">
           <h1 className="text-[#07F8EA] text-3xl">Ideation</h1>
-          <p className="text-[#ccc] text-[13px] mt-4">
+          <p className="text-[#ccc] text-[13px] mt-4 font-normal">
             Dive deep into current tech trends and challenges. Draw inspiration
             from industry leaders, innovative startups, and emerging
             technologies. Use brainstorming sessions to generate a plethora of
@@ -31,7 +34,7 @@ const Schedule = () => {
         </div>
         <div className="absolute bottom-96 -translate-y-40 border border-[#07F8EA] rounded-lg w-[500px] h-[170px] p-4">
           <h1 className="text-[#07F8EA] text-3xl">Announcement</h1>
-          <p className="text-[#ccc] text-[13px] mt-4">
+          <p className="text-[#ccc] text-[13px] mt-4 font-normal">
             After intense brainstorming and collaboration, Best ideas would be
             selected by judges, Their creativity and problem-solving skills have
             set the stage for the next challenge.
@@ -40,7 +43,7 @@ const Schedule = () => {
         </div>
         <div className="absolute right-0 bottom-56 border border-[#07F8EA] rounded-lg w-[500px] h-[180px] p-4">
           <h1 className="text-[#07F8EA] text-3xl">Team</h1>
-          <p className="text-[#ccc] text-[13px] mt-4">
+          <p className="text-[#ccc] text-[13px] mt-4 font-normal">
             Collaboration is key as students bring together their unique skills
             and expertise. Enlist your teams and prepare them to tackle the
             upcoming challenges. Get ready to synergize and innovate with your
@@ -50,7 +53,7 @@ const Schedule = () => {
         </div>
         <div className="absolute bottom-0 translate-y-16 border border-[#07F8EA] rounded-lg w-[500px] h-[180px] p-4">
           <h1 className="text-[#07F8EA] text-3xl">Hackathon</h1>
-          <p className="text-[#ccc] text-[13px] mt-4">
+          <p className="text-[#ccc] text-[13px] mt-4 font-normal">
             Every truly novel concept appears insane at first, so come present
             yours. The time for brainstorming is past; now is the time to code
             and present hackathon presentations.
