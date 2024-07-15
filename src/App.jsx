6566e65d7a12db7e1d507c6287d6a420a2ac29ef
Mohhit6075 <mohitsingh6266@gmail.com>
2024-07-15 14:21:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,8 @@ function App() {
         <>
           <div className="w-[100%] app">
             <div className="w-full h-full ">
-              <video autoPlay muted loop id="video-background" src="/Images/bg9.mp4">
+              <video autoPlay muted loop id="video-background">
+                <source src="/Images/bg9.mp4" type="video/mp4"></source>
               </video>
             </div>
             <Navbar />
