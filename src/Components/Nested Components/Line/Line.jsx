f4a58d052639d8dbd38247af215c3b2486cce237
Mohhit6075 <mohitsingh6266@gmail.com>
2024-08-03 18:17:22 +0530

    Update Line.jsx

--- src/Components/Nested Components/Line/Line.jsx
+++ src/Components/Nested Components/Line/Line.jsx
@@ -7,6 +7,7 @@ const Line = () => {
         viewBox="0 0 1142 2"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        className="w-full mx-auto"
       >
         <path d="M0 1L1142 1" stroke="url(#paint0_linear_386_536)" />
         <defs>
